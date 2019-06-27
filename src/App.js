e3ae67c653e0b50d7202a319d5c9570a0efffbdd
graemeelliott <graeme.elliott@dimedout.com>
2019-06-27 22:53:47 +0100

    Finished background. Connected to Clarifai API. Able to get face boundries from API data.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 // eslint-disable-next-line
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import User from './components/User/User';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+
+/* Particles settings for background */
 
 const particlesOptions = {
   particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 600
+      }
+    },
     line_linked: {
       shadow: {
         enable: true,
@@ -19,20 +31,59 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-    <Particles 
-      params = {particlesOptions}
-    />
-      <Navigation />
-      <Logo />
-      <User />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
+/* Particles settings ends */
 
-    </div>
-  );
+/* Clarifai API Key */
+
+const app = new Clarifai.App({
+  apiKey: 'a93790ffb6ed456e83c9f7139e48f430' 
+ });
+
+ /* Clarifai API Key */
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState ({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState ({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, 
+    this.state.input).then(
+      function(response) {
+        console.log (response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render () {
+    return (
+      <div className="App">
+      <Particles className='particles' 
+        params = {particlesOptions}
+      />
+        <Navigation />
+        <Logo />
+        <User />
+        <ImageLinkForm 
+        onInputChange = {this.onInputChange} 
+        onButtonSubmit = {this.onButtonSubmit}/>
+        <FaceRecognition imageUrl = {this.state.imageUrl} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
